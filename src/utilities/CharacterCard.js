58ec9bf9e1caabfd2f764c0dc6533c53d6580115
Avery Angel <avery.jordan.angel@gmail.com>
2019-10-03 23:05:14 -0700

    styled Character Card a little

--- src/utilities/CharacterCard.js
+++ src/utilities/CharacterCard.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import CharImg from './assets/16Card.png'
 
-function CharacterCard () {
-  return (
-    <div className='charCardContain'>
-      <img className='charImg' src={CharImg} />
-      <div className='charNameContain'>
-        <h4 className='charName'>Android 16</h4>
-      </div>
-    </div>
-  )
-}
+  class CharacterCard extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        charName: 'Android 16'
+      }
+    }
 
+    render () {
+      return (
+        <div className='charCardContain'>
+          <img className='charImg' src={CharImg} alt='character'/>
+          <div className='charNameContain'>
+            <h4 className='charName'>{this.state.charName}</h4>
+          </div>
+        </div>
+      )
+    }
+  }
 export default CharacterCard
