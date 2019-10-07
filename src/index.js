78fac28f0f8073d1d9508e529ee6ff0fb2caba33
Avery Angel <avery.jordan.angel@gmail.com>
2019-10-06 19:38:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers/rootReducer.js';
 import App from './App.js';
+import './index.css'
 
 const store = createStore(rootReducer)
 
